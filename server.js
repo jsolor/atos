f188b001e6434c5bc9a390d44684da99d9535eb9
jsolor <jesus.solorz@gmail.com>
2023-10-13 14:49:06 -0500

    route for a new routine done

--- server.js
+++ server.js
@@ -1,8 +1,42 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, child, get, set } from "firebase/database";
+import makeNewRoutine from "./routine.js";
 
 const app = express();
 
-app.get("/message", (_, res) => res.send("Hello from express!"));
+app.use(express.json());
+
+const firebaseConfig = {
+  databaseURL: "https://atos-9b2e6-default-rtdb.firebaseio.com/"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+app.post('/users/:userId/new', (req, res) => {
+  const dbRef = ref(getDatabase());
+
+  get(child(dbRef, `setup/`))
+    .then((snapshot) => snapshot.val())
+    .then(({workout, schedules}) => makeNewRoutine(req.body.params, workout, schedules))
+    .then((routine) => set(ref(database, `users/${req.params.userId}`), {
+      routine
+    }))
+    .then(() => set(ref(database, `users/${req.params.userId}/lifts`), {
+      primary: req.body.params.primaryLifts,
+      auxiliary: req.body.params.auxiliaryLifts
+    }))
+    .catch((err) => console.log(err));
+}); 
+
+// app.get('/users/:userId/routine', (req, res) => {
+//   const dbRef = ref(getDatabase());
+//
+//   get(child(dbRef, `users/${req.params.userId}/routine`))
+//     .then((snapshot)) => snapshot.val())
+//     .catch((err) => console.log(err));
+// });
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));

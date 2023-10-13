11cbac42e266de0dbca21c54269f96bcac69a471
jsolor <jesus.solorz@gmail.com>
2023-10-13 14:55:53 -0500

    form submission with placeholder user id

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -1,26 +1,58 @@
+import axios from "axios";
+
 function LiftEntry({ category, n }) {
-  const nameID = category + '-' + n + '-name';
-  const weightID = category + '-' + n + '-weight';
+  const nameId = category + '-' + n + '-name';
+  const weightId = category + '-' + n + '-weight';
 
   return (
     <div>
       <label className="label">
         <span className="label-text">#{n}</span>
       </label>
-      <input type="text" placeholder="lift name" name={nameID} className="input input-bordered w-full max-w-xs" />
-      <input type="number" placeholder="training max" name={weightID} className="input input-bordered w-full max-w-xs" />
+      <input type="text" placeholder="lift name" name={nameId} required className="input input-bordered w-full max-w-xs m-0.5" />
+      <input type="number" placeholder="training max" name={weightId} required className="input input-bordered w-full max-w-xs m-0.5" />
     </div>
   );
 }
 
 function Setup() {
+  const submitForm = (e) => {
+    e.preventDefault();
+    
+    const days = e.target.select.value;
+    const primaryLifts = [];
+    const auxiliaryLifts = [];
+
+    for (let i = 0; i < 4; i++) {
+      const prefix = 'pri-' + i;
+      const name = e.target[prefix + '-name'].value;
+      const weight = e.target[prefix + '-weight'].value;
+      primaryLifts.push({ name, weight });
+    }
+    for (let i = 0; i < 6; i++) {
+      const prefix = 'aux-' + i;
+      const name = e.target[prefix + '-name'].value;
+      const weight = e.target[prefix + '-weight'].value;
+      auxiliaryLifts.push({ name, weight });
+    }
+
+    axios.post(`users/${123}/new`, {
+        params: {
+          days,
+          primaryLifts,
+          auxiliaryLifts
+        }
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <form onSubmit={(e) => {e.preventDefault(); console.log(e)}}>
+    <form onSubmit={submitForm}>
       <div className="my-3">
           <a>lift </a>
           {/* make selected reflect current */}
-          <select className="select select-bordered w-full max-w-xs">
+          <select required name="select" className="select select-bordered w-full max-w-xs">
             <option disabled selected></option>
             <option>3</option>
             <option>4</option>
@@ -32,16 +64,14 @@ function Setup() {
 
       <h5>choose primary and auxiliary lifts</h5>
       <div className="join flex justify-between">
-        {/* primary 1-4 auxiliary 1-6 */}
-        {/* use old if old lift tm maybe*/}
         <div className="join-item">
           <h6>primary lifts</h6>
-          {[1,2,3,4].map((i) => <LiftEntry category="pri" n={i}/>)}
+          {[0,1,2,3].map((i) => <LiftEntry category={'pri'} n={i}/>)}
         </div>
 
         <div className="join-item">
           <h6>auxiliary lifts</h6>
-          {[1,2,3,4,5,6].map((i) => <LiftEntry category="aux" n={i}/>)}
+          {[0,1,2,3,4,5].map((i) => <LiftEntry category={'aux'} n={i}/>)}
         </div>
       </div>
       

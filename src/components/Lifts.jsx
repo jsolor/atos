7af70400360e81f48e26a7bd2a9d544135147afc
jsolor <jesus.solorz@gmail.com>
2023-10-16 18:19:22 -0500

    updated layout for day's workouts

--- src/components/Lifts.jsx
+++ src/components/Lifts.jsx
@@ -1,109 +1,86 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-import sampleTMs from '../../sampleData/trainingMaxes.json';
-import sampleSchedules from '../../sampleData/schedules.json';
-import sampleReps from '../../sampleData/reps.json';
-import sampleRecords from '../../sampleData/records.json';
+import sampleRoutine from '../../sampleData/routine.json';
 
-function Lift({ name, weight, reps, target, lastSet }) {
+function SetButton({ reps }) {
+  const [classes, setClasses] = useState('btn btn-active w-12');
+
+  const onClick = (e) => {
+    e.preventDefault();
+    if (classes === 'btn btn-active w-12') {
+      setClasses('btn btn-success w-12');
+    } else {
+      setClasses('btn btn-active w-12')
+    }
+  }
+
   return (
-    <tr>
-      <td></td>
-      <td>{name}</td>
-      <td>{weight}</td>
-      <td>{reps}</td>
-      <td>{target}</td>
-      <td>
-        <input type="checkbox" className="rounded-none"/>
-        <input type="checkbox" className="rounded-none"/>
-        <input type="checkbox" className="rounded-none"/>
-        <input type="checkbox" className="rounded-none"/>
-      </td>
-      <td>
-        <input type="text" placeholder="reps completed on last set" className="input input-ghost w-full max-w-xs" />
-      </td>
-    </tr>
+    <button className={classes} onClick={onClick}>
+      {reps}
+    </button>
+  )
+}
+
+function Lift({ name, weight, reps, lastSet }) {
+  return (
+    <div className="mb-6">
+      <div>
+        <div className="flex justify-between mb-1">
+          <p className="text-xl italic">{name}</p>
+          <p className="text-xl">{weight}</p>
+        </div>
+        <div>
+          <div className="flex justify-center space-x-1">
+            {([0, 1, 2, 3]).map(() => <SetButton reps={reps} />)}
+            <input type="number" className="input w-20" placeholder={lastSet} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-function Lifts() {
-  const primary = 'primary';
-  const auxiliary = 'auxiliary';
-  const routine = 'x6';
-  const day = 0;
-  const week = 0;
-  const [daysLifts, setDaysLifts] = useState(sampleRecords.weeks[week].days[day]);
+function Lifts({ userId, week, day }) {
+  const [routine, setRoutine] = useState([]);
+  const [primaryLifts, setPrimaryLifts] = useState([]);
+  const [auxiliaryLifts, setAuxiliaryLifts] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get(`/users/${userId}/routine`)
+  //     .then(({ data }) => setRoutine(data))
+  //     .catch((err) => console.log(err))
+  // }, [userId]);
 
   useEffect(() => {
-    console.log(daysLifts);
-  }, [daysLifts]);
-
-  /*
-  // generates values for a new routine
+    setRoutine(sampleRoutine);
+  }, []);
+  
   useEffect(() => {
-    //
-    // TODO: 
-    //  reduce redundant code
-    //
-    for (let week = 0; week < sampleReps.primary.intensity.length; week++) {
-      console.log('----------------------------------------------------------------------------------')
-      console.log("WEAK ", week + 1);
-      for (const day in sampleSchedules[routine]) {
-        console.log(`day ${Number(day) + 1}: `);
-        if (primary in sampleSchedules[routine][day]) {
-          const primaryLifts = sampleSchedules[routine][day][primary];
-          for (const lift of primaryLifts) {
-            const weight = sampleReps[primary]['intensity'][week] * sampleTMs[primary][lift];
-            const reps = sampleReps[primary]['reps'][week];
-            const target = sampleReps[primary]['lastSet'][week];
-            console.log(`${lift} @ ${weight} for 4x${reps} & 1x${target}`);
-          }
-        }
-        if (auxiliary in sampleSchedules[routine][day]) {
-          const auxiliaryLifts = sampleSchedules[routine][day][auxiliary];
-          for (const lift of auxiliaryLifts) {
-            const weight = sampleReps[auxiliary]['intensity'][week] * sampleTMs[auxiliary][lift];
-            const reps = sampleReps[auxiliary]['reps'][week] * sampleTMs[auxiliary][lift];
-            const target = sampleReps[auxiliary]['lastSet'][week] * sampleTMs[auxiliary][lift];
-            console.log(`${lift} @ ${weight} for 4x${reps} & 1x${target}`);
-          }
-        }
+    if (routine.length) {
+      if ('primary' in routine[week][day]) {
+      setPrimaryLifts(routine[week][day].primary);
+      } else {
+        setPrimaryLifts([]);
+      }
+      if ('auxiliary' in routine[week][day]) {
+        setAuxiliaryLifts(routine[week][day].auxiliary);
+      } else {
+        setAuxiliaryLifts([]);
       }
     }
-  }, []);
-  */
+  }, [routine, week, day]);
 
   return (
     <div className="overflow-x-auto">
-
-      <table className="table table-zebra">
-
-        <thead>
-          <tr>
-            <th></th>
-            <th>Lift</th>
-            <th>Weight</th>
-            <th>Reps</th>
-            <th>Last Set Target</th>
-            <th>Sets</th>
-            <th>Actual</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {Object.keys(daysLifts).map((lift) =>  (
-            <Lift 
-              name={lift} 
-              weight={daysLifts[lift].weight} 
-              reps={daysLifts[lift].reps} 
-              target={daysLifts[lift].target} 
-              lastSet={daysLifts[lift].lastSet}  
-            />
-          ))}          
-        </tbody>
-
-      </table>
-
+      <div className="divider">primary</div> 
+      {primaryLifts.map(({ name, reps, weight, lastSet }) => 
+        <Lift name={name} reps={reps} weight={weight} lastSet={lastSet}/>
+      )}
+      <div className="divider">auxiliary</div> 
+      {auxiliaryLifts.map(({ name, reps, weight, lastSet }) => 
+        <Lift name={name} reps={reps} weight={weight} lastSet={lastSet}/>
+      )}
     </div>
   );
 }

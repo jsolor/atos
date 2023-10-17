011b4cda9ad4187d87d47ac575226befaa48d2fd
jsolor <jesus.solorz@gmail.com>
2023-10-17 01:21:18 -0500

    added bottom buttons for day & week nav

--- src/components/Lifts.jsx
+++ src/components/Lifts.jsx
@@ -41,8 +41,11 @@ function Lift({ name, weight, reps, lastSet }) {
   );
 }
 
-function Lifts({ userId, week, day }) {
+function Lifts({ userId }) {
   const [routine, setRoutine] = useState([]);
+  const [format, setFormat] = useState(3);
+  const [day, setDay] = useState(0);
+  const [week, setWeek] = useState(0);
   const [primaryLifts, setPrimaryLifts] = useState([]);
   const [auxiliaryLifts, setAuxiliaryLifts] = useState([]);
 
@@ -71,6 +74,40 @@ function Lifts({ userId, week, day }) {
     }
   }, [routine, week, day]);
 
+  useEffect(() => {
+    console.log('day: ' + day);
+  }, [day]);
+
+  useEffect(() => {
+    console.log('week: ' + week);
+  }, [week]);
+
+  const changeWeek = (change) => {
+    if (week + change >= 18) {
+      setWeek(18);
+    } else if (week + change < 0) {
+      setWeek(0);
+    } else {
+      setWeek(week + change);
+    }
+  };
+
+  const changeDay = (change) => {
+    if (day + change >= format) {
+      setDay(0);
+      changeWeek(1);
+    } else if (day + change < 0) {
+      if (day === 0 && week === 0) {
+        setDay(0);
+      } else {
+        setDay(format - 1);
+        changeWeek(-1);
+      }
+    } else {
+      setDay(day + change);
+    }
+  };
+
   return (
     <div className="overflow-x-auto">
       <div className="divider">primary</div> 
@@ -81,6 +118,17 @@ function Lifts({ userId, week, day }) {
       {auxiliaryLifts.map(({ name, reps, weight, lastSet }) => 
         <Lift name={name} reps={reps} weight={weight} lastSet={lastSet}/>
       )}
+      
+      <div className="flex justify-between items-end fixed bottom-0 left-0 right-0 p-4">
+        <div>
+          <button className="btn" onClick={() => changeWeek(-1)}>{'<<'}</button>
+          <button className="btn" onClick={() => changeDay(-1)}>{'<'}</button>
+        </div>
+        <div>
+          <button className="btn" onClick={() => changeDay(1)}>{'>'}</button>
+          <button className="btn" onClick={() => changeWeek(1)}>{'>>'}</button>
+        </div>
+      </div>
     </div>
   );
 }

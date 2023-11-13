5f47887f408d70bee5c638dc3d21e93db884319e
jsolor <jesus.solorz@gmail.com>
2023-11-12 19:57:21 -0600

    added user obj, conditional rendering based on session being active

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,41 @@
 import NavBar from './NavBar';
+import LogIn from './LogIn';
 import Lifts from './Lifts';
 import Profile from './Profile';
 import Settings from './Settings';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [pageBody, setPageBody] = useState('Lifts');
+import { initializeApp } from "firebase/app";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 
-  useEffect(() => {
-    console.log(pageBody);
-  }, [pageBody]);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  databaseURL: import.meta.env.VITE_FIREBASE_URL
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+(async () => {
+  await setPersistence(auth, browserLocalPersistence);
+})();
+
+function App() {
+  const [pageBody, setPageBody] = useState('LogIn');
+  const [user, setUser] = useState(null);
 
   return (
-    <>
-      <NavBar setPageBody={setPageBody} />
+    <div>
+      {user && (<NavBar auth={auth} setUser={setUser} setPageBody={setPageBody} />)}
 
       {
+        (pageBody === 'LogIn' && <LogIn auth={auth} setUser={setUser} setPageBody={setPageBody} />) ||
         (pageBody === 'Lifts' && <Lifts />) ||
         (pageBody === 'Profile' && <Profile />) ||
         (pageBody === 'Settings' && <Settings />)
       }
-    </>
+    </div>
   );
 }
 

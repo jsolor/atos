28fa90d1acb31ba78d1a983b81594425a4861680
jsolor <jesus.solorz@gmail.com>
2023-11-14 17:36:09 -0600

    added uid state variable, passing params to Lifts component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
+import './App.css'
 import NavBar from './NavBar';
 import LogIn from './LogIn';
 import Lifts from './Lifts';
 import Profile from './Profile';
 import Settings from './Settings';
-import { useState } from 'react';
-import './App.css'
-
+import Setup from './Setup';
+import { useEffect, useState } from 'react';
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
 
 const firebaseConfig = {
@@ -16,6 +17,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth();
+const db = getDatabase();
 
 (async () => {
   await setPersistence(auth, browserLocalPersistence);
@@ -24,6 +26,12 @@ const auth = getAuth();
 function App() {
   const [pageBody, setPageBody] = useState('LogIn');
   const [user, setUser] = useState(null);
+  const [uid, setUid] = useState(null);
+
+  useEffect(() => {
+    if (!user) setUid(null);
+    if (user && 'uid' in user) setUid(user.uid);
+  }, [user]);
 
   return (
     <div>
@@ -31,7 +39,7 @@ function App() {
 
       {
         (pageBody === 'LogIn' && <LogIn auth={auth} setUser={setUser} setPageBody={setPageBody} />) ||
-        (pageBody === 'Lifts' && <Lifts />) ||
+        (pageBody === 'Lifts' && <Lifts db={db} uid={uid} />) ||
         (pageBody === 'Profile' && <Profile />) ||
         (pageBody === 'Settings' && <Settings />)
       }

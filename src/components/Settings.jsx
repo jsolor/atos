17e739e6cf8882bf91f71cd29a838bea87eb2050
jsolor <jesus.solorz@gmail.com>
2023-11-16 13:17:51 -0600

    renamed modal

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -22,12 +22,12 @@ function Settings({ db, uid }) {
   return (
     <div>
       {!routineSetup && (<div>
-        <button className="btn" style={deleteStyle} onClick={()=>document.getElementById('my_modal_2').showModal()}>delete routine</button>
-        <dialog id="my_modal_2" className="modal">
+        <button className="btn" style={deleteStyle} onClick={()=>document.getElementById('delete_modal').showModal()}>delete routine</button>
+        <dialog id="delete_modal" className="modal">
           <div className="modal-box">
             <h3 className="font-bold text-lg">Are you sure?</h3>
             <form method="dialog">
-            <button className="btn" onClick={deleteRoutine}>yes</button>
+              <button className="btn" onClick={deleteRoutine}>yes</button>
               {/* if there is a button in form, it will close the modal */}
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             </form>

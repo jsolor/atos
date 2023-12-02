7b62118382788abdb91dc989bafb0682231a397b
jsolor <jesus.solorz@gmail.com>
2023-12-02 14:05:59 -0600

    alternative method of jumping

--- src/components/Lifts.jsx
+++ src/components/Lifts.jsx
@@ -26,7 +26,7 @@ function refreshRoutine(setFormattedRoutine) {
   for (let i = 0; i < 19; i++) {
     routine.push(formatWeek(f + 'x', rawRoutine[i]));
   }
-  console.log('routine refreshed');
+  
   setFormattedRoutine(routine);
 }
 
@@ -138,9 +138,9 @@ function Lifts({ dbRef, uid, week, day, setWeekDay }) {
     }
   };
 
-  const jumpTo = (e) => {
-    const w = Number(e.target.ariaRowIndex);
-    const d = Number(e.target.ariaColIndex);
+  const jumpTo = () => {
+    const w = Number(document.getElementById('jmp-dropdown-week').value);
+    const d = Number(document.getElementById('jmp-dropdown-day').value);
 
     setWeekDay(w, d);
   };
@@ -175,7 +175,7 @@ function Lifts({ dbRef, uid, week, day, setWeekDay }) {
     routine[week][category][index].lastSetActual = val;
     
     localStorage.setItem('routine', JSON.stringify(routine));
-    
+
     refreshRoutine(setFormattedRoutine);
   };
 
@@ -271,22 +271,35 @@ function Lifts({ dbRef, uid, week, day, setWeekDay }) {
       <div className="pb-36"></div>
       <div className="flex flex-row flex-wrap justify-between fixed bottom-0 left-0 right-0 w-screen p-3 mb-3 bg-primary">
         <div className="join xs:order-1">
-          <button className="btn join-item bg-accent border-accent" onClick={() => changeWeek(-1)}>{'<<'}</button>
-          <button className="btn join-item bg-accent border-accent" onClick={() => changeDay(-1)}>{'<'}</button>
+          <button className="btn join-item bg-accent border-accent" onClick={() => changeWeek(-1)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+              <path fill="currentColor" d="M205.66 202.34a8 8 0 0 1-11.32 11.32l-80-80a8 8 0 0 1 0-11.32l80-80a8 8 0 0 1 11.32 11.32L131.31 128ZM51.31 128l74.35-74.34a8 8 0 0 0-11.32-11.32l-80 80a8 8 0 0 0 0 11.32l80 80a8 8 0 0 0 11.32-11.32Z" />
+            </svg>
+          </button>
+          <button className="btn join-item bg-accent border-accent" onClick={() => changeDay(-1)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+              <path fill="currentColor" d="M165.66 202.34a8 8 0 0 1-11.32 11.32l-80-80a8 8 0 0 1 0-11.32l80-80a8 8 0 0 1 11.32 11.32L91.31 128Z" />
+            </svg>
+          </button>
         </div>
         <button className="btn -order-1 xs:order-2 mb-1 xs:mb-0 min-w-full xs:min-w-max bg-accent border-accent" onClick={()=>document.getElementById('jump_modal').showModal()}>jump</button>
         <dialog id="jump_modal" className="modal w-auto">
           <div className="modal-box">
             <form method="dialog">
-              <div className="flex flex-wrap mt-4">
-                {((Array.from({ length: 19 }, (_, index) => index))).map((i) => (
-                  <div key={'jump-week-' + i} className="flex flex-1 mb-1 join w-full min-w-full">
-                    <button className="btn btn-disabled flex-1 join-item p-0">W{i}</button>
-                    {((Array.from({ length: format }, (_, index) => index))).map((j) => 
-                      (<JumpButton key={'jump-' + j} week={week} day={day} i={i} j={j} jumpTo={jumpTo} />)
-                    )}
-                  </div>)
-                )}
+              <div className="flex justify-between space-x-2 mt-4">
+                <select className="select select-bordered w-full max-w-xs" id="jmp-dropdown-week">
+                  <option disabled>Week?</option>
+                  {(Array.from({ length: 19 }, (_, index) => index)).map((i) => 
+                    i === week ? <option key={'jmp-wk-' + i} selected>{i}</option> : <option key={'jmp-wk-' + i}>{i}</option>
+                  )}
+                </select>
+                <select className="select select-bordered w-full max-w-xs" id="jmp-dropdown-day">
+                  <option disabled>Day?</option>
+                  {(Array.from({ length: format }, (_, index) => index)).map((i) => 
+                    i === day ? <option key={'jmp-dy-' + i} selected>{i}</option> : <option key={'jmp-dy-' + i}>{i}</option>
+                  )}
+                </select>
+                <button className="btn" onClick={jumpTo}>go</button>
               </div>
               {/* if there is a button in form, it will close the modal */}
               <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
@@ -294,8 +307,16 @@ function Lifts({ dbRef, uid, week, day, setWeekDay }) {
           </div>
         </dialog>
         <div className="join xs:order-3">
-          <button className="btn join-item bg-accent border-accent" onClick={() => changeDay(1)}>{'>'}</button>
-          <button className="btn join-item bg-accent border-accent" onClick={() => changeWeek(1)}>{'>>'}</button>
+          <button className="btn join-item bg-accent border-accent" onClick={() => changeDay(1)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+              <path fill="currentColor" d="m181.66 133.66l-80 80a8 8 0 0 1-11.32-11.32L164.69 128L90.34 53.66a8 8 0 0 1 11.32-11.32l80 80a8 8 0 0 1 0 11.32Z" />
+            </svg>
+          </button>
+          <button className="btn join-item bg-accent border-accent" onClick={() => changeWeek(1)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256">
+              <path fill="currentColor" d="m141.66 133.66l-80 80a8 8 0 0 1-11.32-11.32L124.69 128L50.34 53.66a8 8 0 0 1 11.32-11.32l80 80a8 8 0 0 1 0 11.32Zm80-11.32l-80-80a8 8 0 0 0-11.32 11.32L204.69 128l-74.35 74.34a8 8 0 0 0 11.32 11.32l80-80a8 8 0 0 0 0-11.32Z" />
+            </svg>
+          </button>
         </div>
       </div>
     </div>

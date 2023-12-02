c1752a1e9cad6b4205238c89693e5fce6c6eba59
jsolor <jesus.solorz@gmail.com>
2023-12-01 21:18:41 -0600

    reduced icon sizes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,14 +3,15 @@ import { signOut } from 'firebase/auth';
 function NavBar({ auth, setUser, pageBody, setPageBody, week, day }) {
   const profileButton = (
     <button className="btn btn-ghost btn-circle" onClick={() => setPageBody('Profile')}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+      <svg className="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
         <path fill="currentColor" d="M172 120a44 44 0 1 1-44-44a44.05 44.05 0 0 1 44 44Zm60 8A104 104 0 1 1 128 24a104.11 104.11 0 0 1 104 104Zm-16 0a88.09 88.09 0 0 0-91.47-87.93C77.43 41.89 39.87 81.12 40 128.25a87.65 87.65 0 0 0 22.24 58.16A79.71 79.71 0 0 1 84 165.1a4 4 0 0 1 4.83.32a59.83 59.83 0 0 0 78.28 0a4 4 0 0 1 4.83-.32a79.71 79.71 0 0 1 21.79 21.31A87.62 87.62 0 0 0 216 128Z"></path>
       </svg>
     </button>
   );
+
   const settingsButton = (
     <button className="btn btn-ghost btn-circle" onClick={() => setPageBody('Settings')}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+      <svg className="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
         <path fill="currentColor" d="M128 80a48 48 0 1 0 48 48a48.05 48.05 0 0 0-48-48Zm0 80a32 32 0 1 1 32-32a32 32 0 0 1-32 32Zm88-29.84q.06-2.16 0-4.32l14.92-18.64a8 8 0 0 0 1.48-7.06a107.21 107.21 0 0 0-10.88-26.25a8 8 0 0 0-6-3.93l-23.72-2.64q-1.48-1.56-3-3L186 40.54a8 8 0 0 0-3.94-6a107.71 107.71 0 0 0-26.25-10.87a8 8 0 0 0-7.06 1.49L130.16 40h-4.32L107.2 25.11a8 8 0 0 0-7.06-1.48a107.6 107.6 0 0 0-26.25 10.88a8 8 0 0 0-3.93 6l-2.64 23.76q-1.56 1.49-3 3L40.54 70a8 8 0 0 0-6 3.94a107.71 107.71 0 0 0-10.87 26.25a8 8 0 0 0 1.49 7.06L40 125.84v4.32L25.11 148.8a8 8 0 0 0-1.48 7.06a107.21 107.21 0 0 0 10.88 26.25a8 8 0 0 0 6 3.93l23.72 2.64q1.49 1.56 3 3L70 215.46a8 8 0 0 0 3.94 6a107.71 107.71 0 0 0 26.25 10.87a8 8 0 0 0 7.06-1.49L125.84 216q2.16.06 4.32 0l18.64 14.92a8 8 0 0 0 7.06 1.48a107.21 107.21 0 0 0 26.25-10.88a8 8 0 0 0 3.93-6l2.64-23.72q1.56-1.48 3-3l23.78-2.8a8 8 0 0 0 6-3.94a107.71 107.71 0 0 0 10.87-26.25a8 8 0 0 0-1.49-7.06Zm-16.1-6.5a73.93 73.93 0 0 1 0 8.68a8 8 0 0 0 1.74 5.48l14.19 17.73a91.57 91.57 0 0 1-6.23 15l-22.6 2.56a8 8 0 0 0-5.1 2.64a74.11 74.11 0 0 1-6.14 6.14a8 8 0 0 0-2.64 5.1l-2.51 22.58a91.32 91.32 0 0 1-15 6.23l-17.74-14.19a8 8 0 0 0-5-1.75h-.48a73.93 73.93 0 0 1-8.68 0a8 8 0 0 0-5.48 1.74l-17.78 14.2a91.57 91.57 0 0 1-15-6.23L82.89 187a8 8 0 0 0-2.64-5.1a74.11 74.11 0 0 1-6.14-6.14a8 8 0 0 0-5.1-2.64l-22.58-2.52a91.32 91.32 0 0 1-6.23-15l14.19-17.74a8 8 0 0 0 1.74-5.48a73.93 73.93 0 0 1 0-8.68a8 8 0 0 0-1.74-5.48L40.2 100.45a91.57 91.57 0 0 1 6.23-15L69 82.89a8 8 0 0 0 5.1-2.64a74.11 74.11 0 0 1 6.14-6.14A8 8 0 0 0 82.89 69l2.51-22.57a91.32 91.32 0 0 1 15-6.23l17.74 14.19a8 8 0 0 0 5.48 1.74a73.93 73.93 0 0 1 8.68 0a8 8 0 0 0 5.48-1.74l17.77-14.19a91.57 91.57 0 0 1 15 6.23L173.11 69a8 8 0 0 0 2.64 5.1a74.11 74.11 0 0 1 6.14 6.14a8 8 0 0 0 5.1 2.64l22.58 2.51a91.32 91.32 0 0 1 6.23 15l-14.19 17.74a8 8 0 0 0-1.74 5.53Z"></path>
       </svg>
     </button>
@@ -31,27 +32,25 @@ function NavBar({ auth, setUser, pageBody, setPageBody, week, day }) {
   };
 
   return (
-    <div>
-      <div className="navbar mx-auto bg-base-100">
+    <div className="navbar bg-base-100">
 
-        <div className="navbar-start">
-          <div className="navbar-start">          
-            {pageBody === 'Profile' && settingsButton}
-            {pageBody !== 'Profile' && profileButton}
-          </div>
+      <div className="navbar-start">
+        <div className="navbar-start">          
+          {pageBody === 'Profile' && settingsButton}
+          {pageBody !== 'Profile' && profileButton}
         </div>
-
-        <div className="navbar-center">
-          <a className="btn btn-ghost normal-case text-xl" onClick={() => setPageBody('Lifts')}>W{week} D{day}</a>
-        </div>
-
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle" title="log out" onClick={logOut}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M232 216h-24V40a16 16 0 0 0-16-16H64a16 16 0 0 0-16 16v176H24a8 8 0 0 0 0 16h208a8 8 0 0 0 0-16ZM192 40v176h-16V40ZM64 40h96v176H64Zm80 92a12 12 0 1 1-12-12a12 12 0 0 1 12 12Z"></path></svg>
-          </button>
-        </div>
-  
       </div>
+
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl" onClick={() => setPageBody('Lifts')}>W{week} D{day}</a>
+      </div>
+
+      <div className="navbar-end">
+        <button className="btn btn-ghost btn-circle" title="log out" onClick={logOut}>
+          <svg className="w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M232 216h-24V40a16 16 0 0 0-16-16H64a16 16 0 0 0-16 16v176H24a8 8 0 0 0 0 16h208a8 8 0 0 0 0-16ZM192 40v176h-16V40ZM64 40h96v176H64Zm80 92a12 12 0 1 1-12-12a12 12 0 0 1 12 12Z"></path></svg>
+        </button>
+      </div>
+
     </div>
   );
 }
